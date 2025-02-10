1a06154540c31d3d3ec2cc4551d89a6f8ca3d63d
Joshua Wei <joshuawei@Joshuas-MacBook-Air-6.local>
2025-02-09 19:13:39 -0800

    Added datasets and code

---
+++ df_EDA.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = pd.read_csv("/Users/joshuawei/air_quality_prediction/clean_df.csv")
+stations = ["6", "43", "88", "211", "233"]
+
+#Station as Categorical
+df["Station"] = df["Station"].astype(str)
+
+#CSV file with Summary Table Grouped by Station
+with open("summary.csv", "w") as f: 
+    for num in stations:
+        station = df[df["Station"] == num]
+        table = station.describe().drop(index=["25%", "50%", "75%"]).round(0)
+        
+        f.write(f"\nSummary Statistics for Station {num}\n")
+        
+        table.to_csv(f)
+
+#Boxplots (Before Removing Outliers)
+fig, axes = plt.subplots(2,4, figsize=(12, 5))
+
+sns.boxenplot(df, x = "DayAirTmpAvg (F)", ax = axes[0,0])
+sns.boxenplot(df, x = "DayDewPnt (F)", ax = axes[1,0])
+sns.boxenplot(df, x = "DayPrecip (in)", ax = axes[0,1])
+sns.boxenplot(df, x = "DayRelHumAvg (%)", ax = axes[1,1])
+sns.boxenplot(df, x = "DaySolRadAvg (Ly/day)", ax = axes[0,2])
+sns.boxenplot(df, x = "DayVapPresAvg (mBars)", ax = axes[1,2])
+sns.boxenplot(df, x = "DayWindRun (MPH)", ax = axes[0,3])
+sns.boxenplot(df, x = "DayWindSpdAvg (MPH)", ax = axes[1,3])
+
+plt.suptitle("Boxplots for Variables (With Outliers)", fontsize=16, fontweight='bold')
+plt.tight_layout()
+plt.show()
+
+#Removing Outliers using IQR Method
+
+outlier_df = df.drop(columns=["DayPrecip (in)"]) #Not removing outliers for this column
+
+for column in outlier_df.columns[3:]:
+    Q1 = np.percentile(df[column].dropna(), 25)
+    Q3 = np.percentile(df[column].dropna(), 75)
+
+    IQR = Q3 - Q1
+
+    lower_bound = Q1 - (1.5 * IQR)
+    upper_bound = Q3 + (1.5 * IQR)
+
+    df = df[(df[column] >= lower_bound) & (df[column] <= upper_bound)]
+    #15202 rows -> 13412 rows
+
+#Boxplots (After Removing Outliers)
+fig, axes = plt.subplots(2,4, figsize=(12, 5))
+
+sns.boxenplot(df, x = "DayAirTmpAvg (F)", ax = axes[0,0])
+sns.boxenplot(df, x = "DayDewPnt (F)", ax = axes[1,0])
+sns.boxenplot(df, x = "DayPrecip (in)", ax = axes[0,1])
+sns.boxenplot(df, x = "DayRelHumAvg (%)", ax = axes[1,1])
+sns.boxenplot(df, x = "DaySolRadAvg (Ly/day)", ax = axes[0,2])
+sns.boxenplot(df, x = "DayVapPresAvg (mBars)", ax = axes[1,2])
+sns.boxenplot(df, x = "DayWindRun (MPH)", ax = axes[0,3])
+sns.boxenplot(df, x = "DayWindSpdAvg (MPH)", ax = axes[1,3])
+
+plt.suptitle("Boxplots for Variables (Without Outliers)", fontsize=16, fontweight='bold')
+plt.tight_layout()
+plt.show()
+
---
+++ df_retrieval.py
@@ -0,0 +1,36 @@
+
+import requests
+import pandas as pd
+import time
+
+api_key = "e11821ce-172c-41ce-a27a-6367972df663"
+base_url = "https://et.water.ca.gov/api/data?"
+
+
+stations = ["6","43","80","87","88","125","211","224","233", "262"]
+date_ranges = [
+    ("2013-01-01", "2016-12-31"),
+    ("2017-01-01", "2020-12-31"),
+    ("2021-01-01", "2024-12-31")
+]
+
+main_df = []
+
+for station in stations: 
+    for start_date, end_date in date_ranges:
+        api_url = f"https://et.water.ca.gov/api/data?appKey={api_key}&targets={station}&startDate={start_date}&endDate={end_date}"
+
+        response = requests.get(api_url)
+        
+        data = response.json()
+
+        records = data["Data"]["Providers"][0]["Records"]
+        df = pd.json_normalize(records)
+        main_df.append(df)
+        
+        time.sleep(1)
+
+final_df = pd.concat(main_df, ignore_index=False)
+
+final_df.to_csv("original.csv", index = False)
+print(len(df))
